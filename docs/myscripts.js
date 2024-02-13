0829d094b567d64ecbbb2e77dfeaeac56d527045
cheung-samantha <samanthacheung00@gmail.com>
2024-02-13 13:08:20 -0800

    added myscripts js file

---
+++ docs/myscripts.js
@@ -0,0 +1,31 @@
+function onHover1() {
+    $("#menuImg1").attr("src", "media/live_blog_thumbnail2.png");
+}
+
+function offHover1() {
+    $("#menuImg1").attr("src", "media/live_blog_thumbnail1.png");
+}
+
+function onHover2() {
+    $("#menuImg2").attr("src", "media/todos_thumbnail2.png");
+}
+
+function offHover2() {
+    $("#menuImg2").attr("src", "media/todos_thumbnail1.png");
+}
+
+function onHover3() {
+    $("#menuImg3").attr("src", "media/yahoo_mail_thumbnail2.png");
+}
+
+function offHover3() {
+    $("#menuImg3").attr("src", "media/yahoo_mail_thumbnail1.png");
+}
+
+function onHover4() {
+    $("#menuImg4").attr("src", "media/discoveryplus_thumbnail2.png");
+}
+
+function offHover4() {
+    $("#menuImg4").attr("src", "media/discoveryplus_thumbnail1.png");
+}

53bf6d0e10ba116853dde59065fa4cb404b01591
cheung-samantha <samanthacheung00@gmail.com>
2024-02-16 17:39:43 -0800

    added discovery case study and new folders for organization

--- docs/myscripts.js
+++ docs/myscripts.js
@@ -1,31 +1,31 @@
 function onHover1() {
-    $("#menuImg1").attr("src", "media/live_blog_thumbnail1.png");
+    $("#menuImg1").attr("src", "media/thumbnails/live_blog_thumbnail1.png");
 }
 
 function offHover1() {
-    $("#menuImg1").attr("src", "media/live_blog_thumbnail2.png");
+    $("#menuImg1").attr("src", "media/thumbnails/live_blog_thumbnail2.png");
 }
 
 function onHover2() {
-    $("#menuImg2").attr("src", "media/todos_thumbnail1.png");
+    $("#menuImg2").attr("src", "media/thumbnails/todos_thumbnail1.png");
 }
 
 function offHover2() {
-    $("#menuImg2").attr("src", "media/todos_thumbnail2.png");
+    $("#menuImg2").attr("src", "media/thumbnails/todos_thumbnail2.png");
 }
 
 function onHover3() {
-    $("#menuImg3").attr("src", "media/yahoo_mail_thumbnail1.png");
+    $("#menuImg3").attr("src", "media/thumbnails/yahoo_mail_thumbnail1.png");
 }
 
 function offHover3() {
-    $("#menuImg3").attr("src", "media/yahoo_mail_thumbnail2.png");
+    $("#menuImg3").attr("src", "media/thumbnails/yahoo_mail_thumbnail2.png");
 }
 
 function onHover4() {
-    $("#menuImg4").attr("src", "media/discoveryplus_thumbnail1.png");
+    $("#menuImg4").attr("src", "media/thumbnails/discoveryplus_thumbnail1.png");
 }
 
 function offHover4() {
-    $("#menuImg4").attr("src", "media/discoveryplus_thumbnail2.png");
+    $("#menuImg4").attr("src", "media/thumbnails/discoveryplus_thumbnail2.png");
 }
